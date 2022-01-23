f0de12adeeeaa3459604f382e6f70aaeba213e6a
FacuBar <efacubar@gmail.com>
2022-01-22 23:42:51 -0300

    9.5 description

--- gopl/exc9-5/main.go
+++ gopl/exc9-5/main.go
@@ -26,7 +26,6 @@ func pp(in, out chan string, respond string) {
 
 // TODO: run benchmarks
 
-// Exercise 9.4: Construct a pipeline that connects an arbitrary number of goroutines
-// with channels. What is the maximum number of pipeline stages you can create
-// without running out of memory? How long does a value take to transit the entire
-// pipeline?
+// Exercise 9.5: Write a program with two goroutines that send messages back and forth
+// over two unbuffered channels in ping-pong fashion. How many communications per
+// second can the program sustain?
